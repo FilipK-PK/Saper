ceb85d9bfa085a988b7ab74772ea87040ef18ace
FilipK-PK <65468985+FilipK-PK@users.noreply.github.com>
2020-06-08 10:38:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
+''' klasa testujaca '''
 import unittest
-from oknoStart import OknoStart
+from okno_start import OknoStart
 
 
-class Test_OknoStart(unittest.TestCase):
-    def test_sprawdzanie_Wartosci_Poczatkowych(self):
-        # moga pojawic sie komunikaty o błedzie które wywołuje funkcja nie testujaca
-        self.assertTrue(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '2', '2', '1'))
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '2', '2', '2'))
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '1', '2', '1'))
+class OknoStartTest(unittest.TestCase):
+    ''' testowanie  '''
+    def pobrane_zmiene_test(self):
+        ''' testowanie zmiennych pobranych z okna start'''
+        # moga pojawic sie komunikaty o bledzie ktore wywoluje funkcja
+        # nie testujaca
+        self.assertTrue(OknoStart.pobrane_zmiene_test(
+            OknoStart, '2', '2', '1'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, '2', '2', '2'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, '1', '2', '1'))
 
-        self.assertTrue(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '15', '15', '75'))
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '15', '16', '15'))
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '15', '15', '76'))
+        self.assertTrue(OknoStart.pobrane_zmiene_test(
+            OknoStart, '15', '15', '75'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, '15', '16', '15'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, '15', '15', '76'))
 
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, 'a', '6', '3'))
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '5', 'b', '2'))
-        self.assertFalse(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '5', 'b', '0'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, 'a', '6', '3'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, '5', 'b', '2'))
+        self.assertFalse(OknoStart.pobrane_zmiene_test(
+            OknoStart, '5', 'b', '0'))
 
-        self.assertTrue(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '5', '5', '2'))
-        self.assertTrue(OknoStart.sprawdzanie_Wartosci_Poczatkowych(OknoStart, '14', '3', '10'))
+        self.assertTrue(OknoStart.pobrane_zmiene_test(
+            OknoStart, '5', '5', '2'))
+        self.assertTrue(OknoStart.pobrane_zmiene_test(
+            OknoStart, '14', '3', '10'))
