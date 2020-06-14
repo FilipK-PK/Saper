dd4db911a04b776c822b816434749b250be7a3ce
FilipK-PK <65468985+FilipK-PK@users.noreply.github.com>
2020-06-14 10:10:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 ''' plik glowny '''
-from okno_start import OknoStart
+import okno_start
 
-def main():
-    gra = OknoStart()
+def main_gra():
+    gra = okno_start()
     gra.uruchom_okna()
 
 
 if __name__ == '__main__':
-    main()
+    main_gra()
