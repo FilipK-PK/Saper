cdb6911c86c7205b793156f9b26c2f1cc6b8bfc0
FilipK-PK <65468985+FilipK-PK@users.noreply.github.com>
2020-06-15 12:27:14 +0200

    Update okno_start_test.py

--- okno_start_test.py
+++ okno_start_test.py
@@ -1,40 +1,40 @@
 ''' klasa testujaca, moga pojawic sie komunikaty o bledzie 
 	ktore wywoluje funkcja nie testujaca'''
 import unittest
-import okno_start
+from okno_start import OknoStart
 
 
 class OknoStartTest(unittest.TestCase):
     def testy_pobrane_zmiene(self):
-        self.assertTrue(okno_start.testy_pobrane_zmiene(
-            okno_start, '2', '2', '1'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '2', '2', '2'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '1', '2', '1'))
+        self.assertTrue(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '2', '2', '1'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '2', '2', '2'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '1', '2', '1'))
 
-        self.assertTrue(okno_start.testy_pobrane_zmiene(
-            okno_start, '15', '15', '75'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '15', '16', '15'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '15', '15', '76'))
+        self.assertTrue(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '15', '15', '75'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '15', '16', '15'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '15', '15', '76'))
 
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, 'a', '6', '3'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '5', 'b', '2'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '5', 'b', '0'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '', 'b', '2'))
-        self.assertFalse(okno_start.testy_pobrane_zmiene(
-            okno_start, '-99', 'b', '0'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, 'a', '6', '3'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '5', 'b', '2'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '5', 'b', '0'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '', 'b', '2'))
+        self.assertFalse(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '-99', 'b', '0'))
 
-        self.assertTrue(okno_start.testy_pobrane_zmiene(
-            okno_start, '5', '5', '2'))
-        self.assertTrue(okno_start.testy_pobrane_zmiene(
-            okno_start, '14', '3', '10'))
+        self.assertTrue(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '5', '5', '2'))
+        self.assertTrue(OknoStart.testy_pobrane_zmiene(
+            OknoStart, '14', '3', '10'))
 
 if __name__ == '__main__':
     unittest.main()
